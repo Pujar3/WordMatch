b3a14941b58e064ec93a0eb00fa0383a2b27cc20
Puja Roy <pujar3@nycstudents.net>
2025-10-17 12:25:44 -0400

    Fixed findBetterGuess

--- src/WordMatch.java
+++ src/WordMatch.java
@@ -12,9 +12,24 @@ public class WordMatch {
     }
     public String findBetterGuess (String guess1,String guess2) {
         if (scoreGuess(guess1) > scoreGuess(guess2))
+        {
             return guess1;
-        if (guess1.compareTo(guess2) > 0) return guess1;
-        return null;
+        }
+        else if (scoreGuess(guess2) > scoreGuess(guess1))
+        {
+            return guess2;
+        }
+        else
+        {
+            if (guess1.compareTo(guess2) > 0)
+            {
+                return guess1;
+            }
+            else
+            {
+                return guess2;
+            }
+        }
     }
 }
 
