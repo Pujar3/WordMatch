b7ddbaf517a35298742f061843f384cc4d60c8f9
Puja Roy <pujar3@nycstudents.net>
2025-12-03 12:26:13 -0500

    Aligned Text

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,20 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) throws IOException {
-            WordMatch w = new WordMatch("mississippi");
-            System.out.println(w.scoreGuess("i"));
-            System.out.println(w.scoreGuess("iss"));
-            System.out.println(w.scoreGuess("issipp"));
-            System.out.println(w.scoreGuess("mississippi"));
-            WordMatch game = new WordMatch("concatenation");
-            System.out.println(game.scoreGuess("ten"));
-            System.out.println(game.scoreGuess("nation"));
-            System.out.println(game.findBetterGuess("ten","nation"));
-            System.out.println(game.scoreGuess("con"));
-            System.out.println(game.scoreGuess("cat"));
-            System.out.println(game.findBetterGuess("con","cat"));
-            System.out.println(WordMatch.read());
-        }
+    public static void main(String[] args) throws IOException {
+        WordMatch w = new WordMatch("mississippi");
+        System.out.println(w.scoreGuess("i"));
+        System.out.println(w.scoreGuess("iss"));
+        System.out.println(w.scoreGuess("issipp"));
+        System.out.println(w.scoreGuess("mississippi"));
+        WordMatch game = new WordMatch("concatenation");
+        System.out.println(game.scoreGuess("ten"));
+        System.out.println(game.scoreGuess("nation"));
+        System.out.println(game.findBetterGuess("ten", "nation"));
+        System.out.println(game.scoreGuess("con"));
+        System.out.println(game.scoreGuess("cat"));
+        System.out.println(game.findBetterGuess("con", "cat"));
+        System.out.println(WordMatch.read());
     }
+}
 
--- src/WordMatch.java
+++ src/WordMatch.java
@@ -4,39 +4,34 @@ import java.util.Scanner;
 
 public class WordMatch {
     private String secret;
-    public WordMatch (String word) {
+
+    public WordMatch(String word) {
         secret = word;
     }
-    public int scoreGuess (String guess) {
+
+    public int scoreGuess(String guess) {
         int score = 0;
-        for(int i = 0; i <= secret.length() - guess.length(); i++)
-            if (secret.substring(i,i + guess.length()).equals(guess))
+        for (int i = 0; i <= secret.length() - guess.length(); i++)
+            if (secret.substring(i, i + guess.length()).equals(guess))
                 score++;
         return score * guess.length() * guess.length();
     }
-    public String findBetterGuess (String guess1,String guess2) {
-        if (scoreGuess(guess1) > scoreGuess(guess2))
-        {
+
+    public String findBetterGuess(String guess1, String guess2) {
+        if (scoreGuess(guess1) > scoreGuess(guess2)) {
             return guess1;
-        }
-        else if (scoreGuess(guess2) > scoreGuess(guess1))
-        {
+        } else if (scoreGuess(guess2) > scoreGuess(guess1)) {
             return guess2;
-        }
-        else
-        {
-            if (guess1.compareTo(guess2) > 0)
-            {
+        } else {
+            if (guess1.compareTo(guess2) > 0) {
                 return guess1;
-            }
-            else
-            {
+            } else {
                 return guess2;
             }
         }
     }
-    public static int read() throws IOException
-    {
+
+    public static int read() throws IOException {
         int points = 0;
         File f = new File(".idea/Guesses.txt");
         Scanner s = new Scanner(f);
